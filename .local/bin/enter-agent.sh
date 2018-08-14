dfd66ae5635142ef3c4aa69382feaa1ba18c65e5
Darren Shepherd <darren@rancher.com>
2018-08-14 16:04:29 -0700

    Updates

--- .local/bin/enter-agent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it rancher-agent /bin/bash
--- .local/bin/start-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~
-~/.local/eclipse/eclipse >/dev/null 2>&1 &
-~/.local/pycharm/bin/pycharm.sh >/dev/null 2>&1 &
