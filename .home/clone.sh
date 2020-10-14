2a8fb4b20564b2b7aa05cc5488e1c4fe32170755
Darren Shepherd <darren@rancher.com>
2020-10-13 21:18:58 -0700

    master branch

--- .home/clone.sh
+++ .home/clone.sh
@@ -2,5 +2,5 @@
 
 rm -rf $HOME/.home
 mkdir $HOME/.home
-git -C $HOME/.home clone --bare https://github.com/ibuildthecloud/home .git
+git -C $HOME/.home clone -b master --bare https://github.com/ibuildthecloud/home .git
 git --git-dir=${HOME}/.home/.git --work-tree=${HOME} reset --hard HEAD
