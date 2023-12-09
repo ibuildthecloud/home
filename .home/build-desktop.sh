00e1810ebfb3a6dd0c8575aa7bf801248a299c0b
Darren Shepherd <darren@acorn.io>
2023-12-08 17:30:47 -0700

    Changes

--- .home/build-desktop.sh
+++ .home/build-desktop.sh
@@ -2,30 +2,18 @@
 set -x -e
 apt update
 apt install -y ca-certificates curl gnupg2
-rm -rf /etc/apt/sources.list.d/*
-echo 'deb http://us.archive.ubuntu.com/ubuntu jammy main universe restricted' > /etc/apt/sources.list
-echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-updates main universe restricted' >> /etc/apt/sources.list
-echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-security main universe restricted' >> /etc/apt/sources.list
-echo 'deb http://us.archive.ubuntu.com/ubuntu jammy-backports main universe restricted' >> /etc/apt/sources.list
-echo 'deb https://apt.syncthing.net/ syncthing stable' >> /etc/apt/sources.list
-echo 'APT::Install-Recommends "false";' > /etc/apt/apt.conf.d/02norecommends
-echo 'APT::Install-Suggests "false";' >> /etc/apt/apt.conf.d/02norecommends
-echo 'APT::Get::Install-Recommends "false";' >> /etc/apt/apt.conf.d/02norecommends
-echo 'APT::Get::Install-Suggests "false";' >> /etc/apt/apt.conf.d/02norecommends
-echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list
-echo 'deb https://download.docker.com/linux/ubuntu jammy stable' >> /etc/apt/sources.list
-echo 'deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian jammy contrib' >> /etc/apt/sources.list
-echo "deb https://ngrok-agent.s3.amazonaws.com buster main" >> /etc/apt/sources.list
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list
-echo "deb https://packages.microsoft.com/repos/ms-teams/ stable main" >> /etc/apt/sources.list
-curl -fsSL https://packages.microsoft.com/repos/ms-teams/dists/stable/Release.gpg | apt-key add -
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | apt-key add -
-curl -fsSL https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-curl -fsSL https://syncthing.net/release-key.gpg | apt-key add -
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-curl -fsSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | apt-key add -
+echo > /etc/apt/sources.list.d/extra.list
+echo "deb [signed-by=/etc/apt/keyrings/syncthing.gpg] https://apt.syncthing.net/ syncthing stable" >> /etc/apt/sources.list.d/extra.list
+echo 'deb [signed-by=/etc/apt/keyrings/spotify.gpg] http://repository.spotify.com stable non-free' >> /etc/apt/sources.list.d/extra.list
+echo 'deb [signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu mantic stable' >> /etc/apt/sources.list.d/extra.list
+echo 'deb [signed-by=/etc/apt/keyrings/oracle.asc] https://download.virtualbox.org/virtualbox/debian jammy contrib' >> /etc/apt/sources.list.d/extra.list
+echo "deb [signed-by=/etc/apt/keyrings/ngrok.asc] https://ngrok-agent.s3.amazonaws.com buster main" >> /etc/apt/sources.list.d/extra.list
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc > /etc/apt/keyrings/oracle.asc
+curl -fsSL https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | gpg --dearmor --yes -o /etc/apt/keyrings/spotify.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://syncthing.net/release-key.gpg > /etc/apt/keyrings/syncthing.gpg
+curl -fsSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc > /etc/apt/keyrings/ngrok.asc
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -38,10 +26,9 @@ apt install -y software-properties-common curl
 
 [ -e slack-desktop.deb ] || curl -fL -o slack-desktop.deb https://downloads.slack-edge.com/releases/linux/4.27.156/prod/x64/slack-desktop-4.27.156-amd64.deb
 [ -e google-chrome.deb ] || curl -fL -o google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-[ -e veracrypt.deb ] || curl -fL -o veracrypt.deb 'https://launchpad.net/veracrypt/trunk/1.24-update3/+download/veracrypt-1.24-Update3-Ubuntu-18.04-amd64.deb'
 [ -e code.deb ] || curl -fL -o code.deb 'https://az764295.vo.msecnd.net/stable/c47d83b293181d9be64f27ff093689e8e7aed054/code_1.42.1-1581432938_amd64.deb'
 [ -e zoom.deb ] || curl -fL -o zoom.deb 'https://zoom.us/client/latest/zoom_amd64.deb'
-[ -e teams.deb ] || curl -fL -o teams.deb 'https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.5.00.10453_amd64.deb'
+[ -e edge.deb ] || curl -fL -o edge.deb 'https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-beta/microsoft-edge-beta_120.0.2210.57-1_amd64.deb?brand=M103'
 
 apt install -y $(cat packages | sort -u) ./*.deb
 apt update -y
